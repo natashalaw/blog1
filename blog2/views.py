d6190a13f2dd1bb030f84d321801a50d92c6c5e3
natashalaw <law.natasha@gmail.com>
2015-09-05 00:07:58 +0100

    ok2

--- blog2/views.py
+++ blog2/views.py
@@ -28,7 +28,7 @@ def add_comment_to_post(request, pk):
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-        	return redirect('blog.views.post_detail', pk=post.pk)
+            return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
